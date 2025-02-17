8204c79287b125a3a5be194db0cce6b8dc221e43
MarcelFox <marcelfox@live.com>
2025-02-17 11:27:32 -0300

    adjusting hello route to return json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get, Json, Router
+};
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct HelloResponse {
+    message: String
+}
+
+
+#[tokio::main]
+async fn main() {
+    let app: Router = Router::new().route("/", get(|| async {
+        return Json(HelloResponse {
+            message: "Hello Cinzia!".to_string()
+        });
+     }));
+    let listener = tokio::net::TcpListener::bind("localhost:3030").await.unwrap();
+    axum::serve(listener, app).await.unwrap()
 }
